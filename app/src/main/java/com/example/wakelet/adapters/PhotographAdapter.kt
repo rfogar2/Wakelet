b51b9c385d73037703a7ba374151290b8849831f
Ryan Fogarty <ryan.fogarty@brighthr.com>
2020-04-22 08:40:00 +0100

    Load images with Glide

--- app/src/main/java/com/example/wakelet/adapters/PhotographAdapter.kt
+++ app/src/main/java/com/example/wakelet/adapters/PhotographAdapter.kt
@@ -3,11 +3,15 @@ package com.example.wakelet.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.wakelet.R
 import com.example.wakelet.models.Photograph
 
@@ -25,6 +29,7 @@ class PhotographAdapter(
     class PhotographViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val container: ConstraintLayout = itemView.findViewById(R.id.container)
         val description: TextView = itemView.findViewById(R.id.description)
+        val image: ImageView = itemView.findViewById(R.id.image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotographViewHolder {
@@ -37,5 +42,10 @@ class PhotographAdapter(
 
         holder.container.setOnClickListener { onClick(photograph) }
         holder.description.text = photograph.description
+
+        Glide.with(holder.image.context)
+            .load(photograph.image)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(holder.image)
     }
 }
